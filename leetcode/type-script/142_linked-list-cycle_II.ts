b1969ab7a59262a20f9b9070f5f99c766ac5ab69
Aidan Lowson <dev@aidanlowson.com>
2024-01-15 18:45:03 +0000

    Linked-list-cycle-II

---
+++ leetcode/type-script/142_linked-list-cycle_II.ts
@@ -0,0 +1,33 @@
+function detectCycle(head: ListNode | null): ListNode | null {
+  if (head === null) {
+    return null;
+  }
+
+  let slow: ListNode | null = head;
+  let fast: ListNode | null = head;
+
+  // Step 1: Determine if there is a cycle
+  while (fast !== null && fast.next !== null) {
+    slow = slow?.next || null;
+    fast = fast.next.next;
+
+    // Cycle detected
+    if (slow === fast) {
+      break;
+    }
+  }
+
+  // No cycle detected
+  if (fast === null || fast.next === null) {
+    return null;
+  }
+
+  // Step 2: Find the start of the cycle
+  while (slow !== fast) {
+    slow = slow?.next || null;
+    fast = fast?.next || null;
+  }
+  slow = head;
+
+  return slow;
+}
