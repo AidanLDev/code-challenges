915d12bc4bda05355f49ff1862a908badcb241a2
Aidan <aidan.lowson@davies-group.com>
2024-02-07 11:39:26 +0000

    Adding maximum depth of binary tree WIP

---
+++ leetcode/type-script/104_maximum-depth-of-binary-tree.ts
@@ -0,0 +1,25 @@
+import { TreeNode } from "../../interfaces/interfaces";
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+
+
+function maxDepth(root: TreeNode | null): number {
+  /*
+    create cur var = root
+    
+    while cur
+  */
+}
