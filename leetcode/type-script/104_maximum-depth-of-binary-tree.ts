10eacd379b51722c0773d9eb3cab357272ce5069
Aidan <aidan.lowson@davies-group.com>
2024-02-07 12:13:27 +0000

    Solution

--- leetcode/type-script/104_maximum-depth-of-binary-tree.ts
+++ leetcode/type-script/104_maximum-depth-of-binary-tree.ts
@@ -17,9 +17,10 @@ import { TreeNode } from "../../interfaces/interfaces";
 
 
 function maxDepth(root: TreeNode | null): number {
-  /*
-    create cur var = root
-    
-    while cur
-  */
+  /* ## Recursive solution ## */
+  if (!root) {
+    return 0;
+  }
+
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
 }
