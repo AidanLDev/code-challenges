0ffaf8a065e7fe22504f85d4c8276eb3a1e6d3a7
AidanLDev <dev@aidanlowson.com>
2024-02-11 22:17:18 +0000

    Adding BFS algo

---
+++ leetcode/type-script/102_binary-tree-level-order-traversal.ts
@@ -0,0 +1,37 @@
+import { TreeNode } from '../../interfaces/interfaces';
+
+function levelOrder(root: TreeNode | null): number[][] {
+  if (root === null) {
+    return [];
+  }
+
+  const result: number[][] = [];
+  let queue: (TreeNode | null)[] = [root];
+
+  while (queue.length > 0) {
+    let queueLength = queue.length;
+    let level: number[] = [];
+
+    for (let i = 0; i < queueLength; i++) {
+      let node = queue.shift();
+
+      if (node !== null) {
+        level.push(node.val);
+
+        if (node.left !== null) {
+          queue.push(node.left);
+        }
+
+        if (node.right !== null) {
+          queue.push(node.right);
+        }
+      }
+    }
+
+    if (level.length > 0) {
+      result.push(level);
+    }
+  }
+
+  return result;
+}
