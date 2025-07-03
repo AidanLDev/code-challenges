1a86b6052c20f535147f1447ac1bab194911e251
Aidan <dev@aidanlowson.com>
2025-07-03 08:39:21 +0100

    problem 1.4

--- CTCI/chap-1/Main.java
+++ CTCI/chap-1/Main.java
@@ -71,8 +71,61 @@ public class Main {
         removeDupesInPlace(word3));
   }
 
+  // 1.4 Write a method to decide if two strings are anagrams or not
+  static boolean isAnagram(String originalWord, String wordToCheck) {
+    for (int i = 0; i < originalWord.length(); i++) {
+      char letterToCheck = originalWord.charAt(i);
+      int wordToCheckIdx = wordToCheck.indexOf(letterToCheck);
+      if (wordToCheckIdx == -1) {
+        return false;
+      }
+      StringBuilder sb = new StringBuilder(wordToCheck);
+      sb.deleteCharAt((wordToCheckIdx));
+      wordToCheck = sb.toString();
+      System.out.println("wordToCheck after delete: " + wordToCheck);
+    }
+    System.out.println("wordToCheck after algo: " + wordToCheck);
+    if (wordToCheck == "") {
+      System.out.println("word to check is null...");
+      return true;
+    }
+    return false;
+  }
+
+  // 1.4 tests
+  static void testIsAnagram() {
+    String name1 = "aidan";
+    String name2 = "nadia";
+    String name3 = "benny";
+    String name4 = "jenny";
+
+    System.out.println("Is " + name1 + " an anagram of: " + name2 + "? " + isAnagram(name1, name2));
+    System.out.println("Is " + name3 + " an anagram of: " + name4 + "? " + isAnagram(name3, name4));
+  }
+
+  // 1.5 Write a method to replace all spaces in a string with '%20'
+
+  // 1.5 tests
+
+  // 1.6 Given an image represented by an NxN matrix, where each pixel in the
+  // image is 4 bytes, write a method to rotate the image by 90 degrees. Can you
+  // do this in place?
+
+  // 1.6 tests
+
+  // 1.7 If an element in MxN matrix is 0, the entire row and column is set to 0
+
+  // 1.7 tests
+
+  // 1.8 Assume you have a method isSubstring which checks if one word is a
+  // substring of another. Given two strings, s1 and s2, write code to check if s2
+  // is a rotation of s1 using only one call to isSubstring (i.e. "waterbottle",
+  // "erbottlewat")
+
+  // 1.8 tests
+
   public static void main(String[] args) {
-    testRemoveDupesInPlace();
+    testIsAnagram();
   }
 
 }
