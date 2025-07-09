77c76901d3648c62b63b4c3b17f9edae26d349cc
Aidan <dev@aidanlowson.com>
2025-07-09 07:49:08 +0100

    LinkedList questions

--- CTCI/chapter-2-linked-lists/LinkedList.java
+++ CTCI/chapter-2-linked-lists/LinkedList.java
@@ -41,24 +41,43 @@ public class LinkedList {
     }
   }
 
-  public static void main(String[] args) {
-    LinkedList list = new LinkedList();
-    Node head = list.new Node(1);
+  public static void addDataToList(Node head) {
     head.appendToTail(2);
     head.appendToTail(3);
     head.appendToTail(4);
 
     System.out.println("OriginalList: ");
     head.printList();
+  }
 
-    head = head.deleteNode(head, 3);
-    System.out.println("List after deleting 3");
-    head.printList();
+  // 2.1 write code to remove duplicates from an unsorted list
+  // 2.1.1 (follow up) how would you solve this problem if a temporary buffer is
+  // not allowed?
 
-    head = head.deleteNode(head, 1);
-    System.out.println("List after deleting 1");
-    head.printList();
+  // 2.2 Implement an algorithm to find the nth to last element of a singly linked
+  // list
 
+  // 2.3 Implement an algorithm to delete a node in the middle of a single linked
+  // list, given only access to that node
+  // 2.3 Example - Input the node 'c' from the linked list a->b->c->d->e
+  // 2.3 Result - Nothing returned but new linked list looks like a->b->d->e
+
+  // 2.4 You have two numbers represented by a linked list where each node
+  // contains a single digit. The digits are sorted in reverse order, such that
+  // the 1's digit is at the head of the list. Write a function that adds two
+  // numbers and returns the sum as a linked list
+  // 2.4 Example - input (3->1->5)+(5->9->2)
+  // Result - 8->0->8
+
+  // 2.5 Given a circular linked list, implement an algorithm which returns node
+  // at the beginning of the loop
+  // 2.5 Example - a->b->c->d->e->c(same c as earlier)
+  // 2.5 Result - c
+
+  public static void main(String[] args) {
+    LinkedList list = new LinkedList();
+    Node head = list.new Node(1);
+    addDataToList(head);
   }
 
 }
