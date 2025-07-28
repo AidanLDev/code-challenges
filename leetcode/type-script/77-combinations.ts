fec3d2780eb9c26bfea61bdd842437cbc45fdb29
Aidan Lowson <dev@aidanlowson.com>
2025-07-28 11:34:32 +0100

    Adding comments to make more sense of backtracking

--- leetcode/type-script/77-combinations.ts
+++ leetcode/type-script/77-combinations.ts
@@ -1,4 +1,6 @@
 export function combine(n: number, k: number): number[][] {
+  // N = range of numbers we want to find e.g. N = 4 [1, 2], [2, 3], [3, 4] (find all nums from 1 -> n within the range of k)
+  // K = Length of our combination e.g. K = 2 [num1, num2]
   if (k > n) {
     return [];
   }
@@ -9,13 +11,15 @@ export function combine(n: number, k: number): number[][] {
 
   function backtrack(start: number, path: number[]) {
     if (path.length === k) {
+      // Base case, if we've built up a combination the size of k, add combination to our results array
       combinations.push([...path]);
       return;
     }
     for (let i = start; i <= n; i++) {
-      path.push(i);
-      backtrack(i + 1, path);
-      path.pop();
-    }
+      // Loop until we reach n
+      path.push(i); // Build our combination
+      backtrack(i + 1, path); // Check if we've finished i.e. path is the same length as k
+      path.pop(); // after our recursive function returns, pop so we can do the next combination
+    } // Once the loop finished, we return and go onto the next iteration of numbers
   }
 }
