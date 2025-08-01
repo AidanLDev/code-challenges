495c5ec76ff52d73d57eb465ceb1cd20687a61d3
Aidan <dev@aidanlowson.com>
2025-08-01 07:45:53 +0100

    Starting min stack q

--- CTCI/chapter-3-stacks-n-queues/CTCI_Chapter_3_Stacks_N_Queues.ts
+++ CTCI/chapter-3-stacks-n-queues/CTCI_Chapter_3_Stacks_N_Queues.ts
@@ -105,6 +105,28 @@ class SingleArrayThreeStacks {
 
 export { SingleArrayThreeStacks };
 // 3.2 How would you design a stack which, in addition to push and pop, also has a function min which returns the minimum element? Push, pop and min should all operate in o(1) time.
+class TrackCurMin {
+  private stack: number[];
+  private minStack: number[];
+  private curMin: number = Infinity;
+
+  constructor() {
+    this.stack = [];
+    this.minStack = [];
+  }
+
+  isEmpty() {
+    return this.stack.length === 0;
+  }
+
+  push(num: number) {
+    if (num < this.curMin) {
+      this.curMin = num;
+      this.minStack.push(num);
+    }
+    this.stack.push(num);
+  }
+}
 
 /* 3.3. Imagine a (literal) stack of plates. If the stack gets too high, it might topple. Therefore in real life, we would likely start a new stack when the previous stack exceeds some threshold. Implement a data structure SetOfStacks that mimics this. SetOfStacks should be composed of several stacks, and should create a new stack once the previous one exceeds capacity. SetOfStacks.push() and SetOfStacks.pop() should behave identically to a single stack (that is, pop( should return the same value as it would if there is just 1 stack))
 
