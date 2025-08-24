8043399eabbe2497b640fc6b6f603882845c81d6
Aidan Lowson <dev@aidanlowson.com>
2025-08-24 14:28:10 +0100

    Finishing the cars question

--- leetcode/type-script/583_car-fleet.js
+++ leetcode/type-script/583_car-fleet.js
@@ -2,10 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.carFleet = carFleet;
 function carFleet(target, position, speed) {
-    var fleets = 0;
     var cars = position.map(function (pos, i) { return [pos, speed[i]]; });
     cars.sort(function (a, b) { return a[0] - b[0]; });
+    var stack = [];
     console.log("cars: ", cars);
-    return fleets;
+    for (var i = cars.length - 1; i > 0; i--) {
+        var _a = cars[i], position_1 = _a[0], speed_1 = _a[1];
+        var timeToTarget = (target - position_1) / speed_1;
+        if (stack.length === 0 || timeToTarget > stack[stack.length - 1]) {
+            stack.push(timeToTarget);
+        }
+    }
+    return stack.length;
 }
-carFleet(10, [3, 5, 7], [3, 2, 1]);
+var res = carFleet(10, [3, 5, 7], [3, 2, 1]);
+console.log("Amount of fleets: ", res);
