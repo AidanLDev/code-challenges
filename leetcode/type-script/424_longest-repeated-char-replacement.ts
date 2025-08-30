2dbe2567a90ba45ad05735e34518523b990d3992
Aidan Lowson <dev@aidanlowson.com>
2025-08-30 19:41:46 +0100

    Starting longest repeated char replacement

---
+++ leetcode/type-script/424_longest-repeated-char-replacement.ts
@@ -0,0 +1,17 @@
+/**
+ * @description You are given a string s consisting of only uppercase english characters and an integer k. You can choose up to k characters of the string and replace them with any other uppercase English character.
+ *  After performing at most k replacements, return the length of the longest substring which contains only one distinct character.
+ * @param s upper case chars
+ * @param k the amount of chars we can convert
+ * @returns the number of the longest repeated character in the array
+ */
+export function longestCharReplacement(s: string, k: number): number {
+  let curLongest = 0;
+
+  let l = 0;
+  let r = 0;
+
+  while (r < s.length) {}
+
+  return curLongest;
+}
