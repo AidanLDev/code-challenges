75f91054d43192d2f4c37b4bbf29bd524c751b7c
Aidan Lowson <dev@aidanlowson.com>
2025-09-04 18:58:08 +0100

    Going through isbalanced again

--- leetcode/type-script/110_balanced-binary-tree.ts
+++ leetcode/type-script/110_balanced-binary-tree.ts
@@ -11,7 +11,7 @@
  *     }
  * }
  */
-import { TreeNode } from '../../interfaces/interfaces';
+import { TreeNode } from "../../interfaces/interfaces";
 function isBalanced(root: TreeNode | null): boolean {
   function dfs(root: TreeNode | null): any[] {
     if (!root) {
@@ -23,4 +23,18 @@ function isBalanced(root: TreeNode | null): boolean {
     return [balance, 1 + Math.max(left[1], right[1])];
   }
   return dfs(root)[0];
+}
+
+function isBalancedTwo(root: TreeNode | null): boolean {
+  if (!root) return true;
+
+  function traverse(curNode: TreeNode | null): [boolean, number] {
+    if (!curNode) return [true, 0];
+    const left = traverse(curNode.left || null);
+    const right = traverse(curNode.right || null);
+    const curBalanced =
+      left[0] && right[0] && Math.abs(left[1] - right[1]) <= 1;
+    return [curBalanced, 1 + Math.max(left[1], right[1])];
+  }
+  return traverse(root)[0];
 }
