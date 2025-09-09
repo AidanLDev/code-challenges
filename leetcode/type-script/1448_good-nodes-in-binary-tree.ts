2e010f7f7335a23f1ebec515866fce68cd4ebcaa
Aidan Lowson <aidan.lowson@processvision.com>
2025-09-09 11:30:49 +0100

    Refactor goodNodes function to simplify traversal logic and improve readability

--- leetcode/type-script/1448_good-nodes-in-binary-tree.ts
+++ leetcode/type-script/1448_good-nodes-in-binary-tree.ts
@@ -5,9 +5,16 @@ import { TreeNode } from "../../interfaces/interfaces";
 Return the number of good nodes in the binary tree. */
 function goodNodes(root: TreeNode | null): number {
   if (!root) return 0;
-  let goodNodes = 1;
-  function traverse(curNode: TreeNode | null, curPath: number[]) {
-    
+  function traverse(curNode: TreeNode | null, maxSoFar: number): number {
+    if (!curNode) return 0;
+    if (!root) return 0;
+    let good = curNode.val >= maxSoFar ? 1 : 0;
+    let newMax = Math.max(maxSoFar, curNode.val);
+    return (
+      good +
+      traverse(curNode.left || null, newMax) +
+      traverse(curNode.right || null, newMax)
+    );
   }
-  return goodNodes;
+  return traverse(root, root.val);
 }
