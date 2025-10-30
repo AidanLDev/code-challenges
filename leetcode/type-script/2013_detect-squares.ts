f869b5081afafddfa6ed926125ac2a78b0bcf34e
Aidan Lowson <dev@aidanlowson.com>
2025-10-30 14:45:02 +0000

    Refactor DetectSquares class to initialize pointCount and improve add and count methods

--- leetcode/type-script/2013_detect-squares.ts
+++ leetcode/type-script/2013_detect-squares.ts
@@ -1,13 +1,40 @@
 class DetectSquares {
-  private readonly points: number[][] = [];
-  constructor() {}
+  private readonly pointCount;
+  private readonly points: number[][]
+  constructor() {
+    this.pointCount = new Map();
+    this.points = [];
+  }
 
+  /**
+   * @param {number[]} point
+   * @return {void}
+   */
   add(point: number[]): void {
+    const p = point.join(",");
+    this.pointCount.set(p, (this.pointCount.get(p) || 0) + 1);
     this.points.push(point);
   }
 
+  /**
+   * @param {number[]} point
+   * @return {number}
+   */
   count(point: number[]): number {
-    // Find all points that are of equal distance to the left or right, top or bottom of this point
-    
+    let res = 0;
+    const [pointX, pointY] = point;
+    for (const [x, y] of this.points) {
+      if (
+        Math.abs(pointX - x) !== Math.abs(pointY - y) ||
+        x === pointX ||
+        y === pointY
+      ) {
+        continue;
+      }
+      res +=
+        (this.pointCount.get(`${x},${pointY}`) || 0) *
+        (this.pointCount.get(`${pointX},${y}`) || 0);
+    }
+    return res;
   }
 }
